5c25ec3a07fb49024539c6a76e82622559e207de
Amy Kirasack <amykirasack@gmail.com>
2018-11-08 22:43:29 +0100

    refactor: Try to refactor logic to persist in background

--- client/public/background.js
+++ client/public/background.js
@@ -1,12 +1,30 @@
 /*global chrome*/
+// import moment from './moment.js';
+// var { moment } = require('./moment.js');
 
-var timerDisp = {
-  count: 0
+// var timerDisp = {
+//   count: 0
+// };
+
+// setInterval(() => {
+//   console.log('yay it works 👾', timerDisp.count);
+//   timerDisp.count++;
+// }, 1000);
+
+var STATUSES = {
+  NOT_SET: 'NOT_SET',
+  TIMER_RUNNING: 'TIMER_RUNNING',
+  TIMER_PAUSED: 'TIMER_PAUSED',
+  POMO_COMPLETE: 'POMO_COMPLETE'
+};
+
+var timer = {
+  countdownID: null,
+  remaining: moment.duration(9, 'seconds'),
+  timerStatus: STATUSES.NOT_SET,
+  pomoCount: 0
 };
 
 setInterval(() => {
-  console.log('yay it works 👾', timerDisp.count);
-  timerDisp.count++;
+  console.log('timer.remaining', timer.remaining);
 }, 1000);
-
-console.log('-----', window);
--- client/src/App.js
+++ client/src/App.js
@@ -7,82 +7,126 @@ import './App.css';
 import TimerDisplay from './components/TimerDisplay';
 import TimerButton from './components/TimerButton';
 
-// const background = chrome.extension.getBackgroundPage();
-// setInterval(() => {
-//   console.log('------------');
-//   console.log('background', background.timerDisplay.count);
-//   console.log('------------');
-// }, 1000);
-// background.timerDisplay.count
-
 class App extends Component {
   state = {
     pomoDuration: moment.duration(6, 'seconds'),
     shortBreakDuration: moment.duration(3, 'seconds'),
     longBreakDuration: moment.duration(10, 'seconds'),
-    timerDisplay: moment.duration(6, 'seconds'),
-    timerStatus: STATUSES.NOT_SET,
-    countdown: null,
-    pomoCounter: 0,
-    background: null
+    // countdownID: null,
+    // timerDisplay: moment.duration(6, 'seconds'),
+    // timerStatus: STATUSES.NOT_SET,
+    // pomoCounter: 0,
+    countdownID: chrome.extension.getBackgroundPage().timer.countdownID,
+    timerDisplay: chrome.extension.getBackgroundPage().timer.remaining,
+    timerStatus: chrome.extension.getBackgroundPage().timer.timerStatus,
+    pomoCounter: chrome.extension.getBackgroundPage().timer.pomoCount,
+    background: chrome.extension.getBackgroundPage()
   };
 
-  componentWillMount() {
-    this.setState(() => ({
-      background: chrome.extension.getBackgroundPage()
-    }));
+  componentDidMount() {
+    setInterval(
+      () =>
+        this.setState({
+          background: chrome.extension.getBackgroundPage()
+        }),
+      500
+    );
   }
 
   toggleTimer = () => {
+    const { background } = this.state;
     if (this.state.timerStatus !== 'TIMER_RUNNING') {
-      this.setState({
-        timerStatus: STATUSES.TIMER_RUNNING,
-        countdown: setInterval(this.reduceTimer, 1000)
-      });
+      background.timer.timerStatus = background.STATUSES.TIMER_RUNNING;
+      background.timer.countdownID = setInterval(this.reduceTimer, 1000);
     } else {
-      clearInterval(this.countdown);
-      this.setState({
-        timerStatus: STATUSES.TIMER_PAUSED,
-        countdown: clearInterval(this.state.countdown)
-      });
+      background.timer.countdownID = clearInterval(
+        background.timer.countdownID
+      );
+      background.timer.timerStatus = background.STATUSES.TIMER_PAUSED;
     }
   };
 
+  // toggleTimer = () => {
+  //   if (this.state.timerStatus !== 'TIMER_RUNNING') {
+  //     this.setState({
+  //       timerStatus: STATUSES.TIMER_RUNNING,
+  //       countdownID: setInterval(this.reduceTimer, 1000)
+  //     });
+  //   } else {
+  //     clearInterval(this.countdownID);
+  //     this.setState({
+  //       timerStatus: STATUSES.TIMER_PAUSED,
+  //       countdownID: clearInterval(this.state.countdownID)
+  //     });
+  //   }
+  // };
+
   resetTimer = () => {
-    this.setState({
-      timerStatus: STATUSES.NOT_SET,
-      timerDisplay: this.state.pomoDuration,
-      countdown: clearInterval(this.state.countdown),
-      pomoCounter: 0
-    });
+    const { background } = this.state;
+
+    background.timer.timerStatus = background.STATUSES.NOT_SET;
+    background.timer.countdownID = clearInterval(background.timer.countdownID);
+    background.timer.remaining = this.state.pomoDuration;
+    background.timer.pomoCount = 0;
   };
 
+  // resetTimer = () => {
+  //   this.setState({
+  //     timerStatus: STATUSES.NOT_SET,
+  //     timerDisplay: this.state.pomoDuration,
+  //     countdownID: clearInterval(this.state.countdownID),
+  //     pomoCounter: 0
+  //   });
+  // };
+
   reduceTimer = () => {
-    let { pomoCounter } = this.state;
+    const { background } = this.state;
 
     const timerFinished =
-      this.state.timerDisplay.get('minutes') === 0 &&
-      this.state.timerDisplay.get('seconds') === 0;
+      background.timer.remaining.get('minutes') === 0 &&
+      background.timer.remaining.get('seconds') === 0;
 
     if (timerFinished) {
-      this.setState({
-        countdown: clearInterval(this.state.countdown),
-        pomoCounter: ++pomoCounter,
-        timerStatus: STATUSES.NOT_SET
-      });
+      background.timer.countdownID = clearInterval(
+        background.timer.countdownID
+      );
+      background.timer.pomoCount = ++background.timer.pomoCount;
+      background.timer.timerStatus = background.STATUSES.NOT_SET;
       this.onTimerEnd();
       return;
     }
 
-    const timerDisplay = moment.duration(this.state.timerDisplay);
+    const timerDisplay = moment.duration(background.timer.remaining);
     timerDisplay.subtract(1, 'second');
-    this.setState({ timerDisplay });
+    background.timer.remaining = timerDisplay;
   };
 
-  onTimerEnd = () => {
-    const { pomoCounter } = this.state;
+  // reduceTimer = () => {
+  //   let { pomoCounter } = this.state;
 
-    if (pomoCounter === 8) {
+  //   const timerFinished =
+  //     this.state.timerDisplay.get('minutes') === 0 &&
+  //     this.state.timerDisplay.get('seconds') === 0;
+
+  //   if (timerFinished) {
+  //     this.setState({
+  //       countdownID: clearInterval(this.state.countdownID),
+  //       pomoCounter: ++pomoCounter,
+  //       timerStatus: STATUSES.NOT_SET
+  //     });
+  //     this.onTimerEnd();
+  //     return;
+  //   }
+
+  //   const timerDisplay = moment.duration(this.state.timerDisplay);
+  //   timerDisplay.subtract(1, 'second');
+  //   this.setState({ timerDisplay });
+  // };
+
+  onTimerEnd = () => {
+    const { timer } = this.state.background;
+
+    if (timer.pomoCount === 8) {
       this.completePomo();
     } else {
       this.setTimerCycle();
@@ -90,43 +134,73 @@ class App extends Component {
     }
   };
 
-  setTimerCycle = () => {
-    let {
-      pomoCounter,
-      pomoDuration,
-      shortBreakDuration,
-      longBreakDuration
-    } = this.state;
+  // onTimerEnd = () => {
+  //   const { pomoCounter } = this.state;
 
-    if (pomoCounter % 2 === 0) {
-      this.setState({
-        timerDisplay: pomoDuration
-      });
+  //   if (pomoCounter === 8) {
+  //     this.completePomo();
+  //   } else {
+  //     this.setTimerCycle();
+  //     this.toggleTimer();
+  //   }
+  // };
+
+  setTimerCycle = () => {
+    const { timer } = this.state.background;
+    const { pomoDuration, shortBreakDuration, longBreakDuration } = this.state;
+
+    if (timer.pomoCount % 2 === 0) {
+      timer.remaining = pomoDuration;
     } else {
-      pomoCounter < 7
-        ? this.setState({
-            timerDisplay: shortBreakDuration
-          })
-        : this.setState({
-            timerDisplay: longBreakDuration
-          });
+      timer.pomoCount < 7
+        ? (timer.remaining = shortBreakDuration)
+        : (timer.remaining = longBreakDuration);
     }
   };
 
+  // setTimerCycle = () => {
+  //   let {
+  //     pomoCounter,
+  //     pomoDuration,
+  //     shortBreakDuration,
+  //     longBreakDuration
+  //   } = this.state;
+
+  //   if (pomoCounter % 2 === 0) {
+  //     this.setState({
+  //       timerDisplay: pomoDuration
+  //     });
+  //   } else {
+  //     pomoCounter < 7
+  //       ? this.setState({
+  //           timerDisplay: shortBreakDuration
+  //         })
+  //       : this.setState({
+  //           timerDisplay: longBreakDuration
+  //         });
+  //   }
+  // };
+
   completePomo = () => {
-    const { pomoDuration } = this.state;
-    this.setState({
-      timerStatus: STATUSES.POMO_COMPLETE,
-      timerDisplay: pomoDuration,
-      pomoCounter: 0
-    });
-    return;
+    const { background } = this.state;
+
+    background.timer.timerStatus = background.STATUSES.POMO_COMPLETE;
+    clearInterval(background.timer.countdownID);
+    background.timer.remaining = this.state.pomoDuration;
+    background.timer.pomoCount = 0;
   };
 
+  // completePomo = () => {
+  //   const { pomoDuration } = this.state;
+  //   this.setState({
+  //     timerStatus: STATUSES.POMO_COMPLETE,
+  //     timerDisplay: pomoDuration,
+  //     pomoCounter: 0
+  //   });
+  //   return;
+  // };
+
   render() {
-    // setInterval(() => {
-    //   console.log('hi this is the background', this.state.background.aa);
-    // }, 1000);
     const { timerDisplay, timerStatus, pomoCounter, background } = this.state;
     return (
       <div className="App">
@@ -137,7 +211,6 @@ class App extends Component {
           </span>
           PomoCounter: {this.state.pomoCounter}
         </p>
-        <h1>BG Count: {background.timerDisp.count}</h1>
         <TimerDisplay
           timerDisplay={timerDisplay}
           timerStatus={timerStatus}
