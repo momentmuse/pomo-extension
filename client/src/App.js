47f5c6cbb5e80ebbc5011bcc1beac8af645d3b8b
Amy Kirasack <amykirasack@gmail.com>
2018-11-12 11:10:07 +0100

    chore: Comment code in App to explain reasoning behind code structure

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,13 @@ import BlockForm from './components/Options/BlockForm';
 class App extends Component {
   state = {
     display: 'timer',
+    // if I had more time, I would set up an event listener and handler to track changes in background.js
     background: chrome.extension.getBackgroundPage()
   };
 
+  // current mode of communication between App and background relies on reference to mutating the same object in memory--which undermines the design principles of React!
+  // App.state is not set up to handle and reset state upon background mutation
+  // thus, I need to repeatedly reset the state to force re-render
   componentDidMount() {
     setInterval(() => {
       this.setState(this.state);
